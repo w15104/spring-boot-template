348ed977ee3be019f898b68055ce6071c77e8e71
huhuhulala <Administrator@greenvm-y15649v1>
2019-03-15 11:39:45 +0800

    提交junit接口测试示例

---
+++ deos-test/src/test/java/com/demo/deos/TestApiExample.java
@@ -0,0 +1,43 @@
+package com.demo.deos;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import com.w15104.demo.study.App;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 接口测试
+ * @author yqz
+ */
+@Slf4j
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class) // 指定SpringBoot启动类
+public class TestApiExample {
+
+	/*接口*/
+	private static final String api = "/v1/classmate/get/1";
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testApi() throws Exception {
+		log.info("test api");
+		mockMvc.perform(get(api).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andDo(print())
+		.andReturn();
+	}
+	
+}
