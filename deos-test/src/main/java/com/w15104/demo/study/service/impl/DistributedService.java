ac03aad5294b4927e27759033510e0e977877b83
w15104 <1434613751@qq.com>
2019-03-25 14:45:33 +0800

    修改统一返回信息，删除英文返回信息，只保留中文

--- deos-test/src/main/java/com/w15104/demo/study/service/impl/DistributedService.java
+++ deos-test/src/main/java/com/w15104/demo/study/service/impl/DistributedService.java
@@ -21,14 +21,14 @@ public class DistributedService implements IDistributedService {
 	@Override
 	public void serviceAToServiceB(String args) {
 		
-		/**
-		 * 1. 检查参数是否合法
-		 * 2. 该操作流程是否需要对数据库进行增、删、改的操作（查询操作不做约束）
-		 * 3. 如果两个服务操作的数据没有直接的关系则优先对serviceA进行操作，之后再对serviceB进行操作
-		 *    优点：a. 如果serviceA操作失败，可直接返回，结束流程，如果成功则可以发起对serviceB的调用。
-		 *            serviceB（成功/失败）返回到serviceA，失败的话serviceA进行回滚
-		 *    如果先对serviceB进行操作，则当serviceA操作失败后，还要再次访问serviceB做回滚（更新表）操作。
-		 *  
+		/*
+		  1. 检查参数是否合法
+		  2. 该操作流程是否需要对数据库进行增、删、改的操作（查询操作不做约束）
+		  3. 如果两个服务操作的数据没有直接的关系则优先对serviceA进行操作，之后再对serviceB进行操作
+		     优点：a. 如果serviceA操作失败，可直接返回，结束流程，如果成功则可以发起对serviceB的调用。
+		             serviceB（成功/失败）返回到serviceA，失败的话serviceA进行回滚
+		     如果先对serviceB进行操作，则当serviceA操作失败后，还要再次访问serviceB做回滚（更新表）操作。
+
 		 */
 
 	}
